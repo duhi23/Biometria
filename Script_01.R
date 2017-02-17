3d1b38c15eb0db080a18b066815421e4a7aceaa6
duhi23 <diego.21.phs@hotmail.com>
2017-02-17 13:35:17 +0100

    Code R

---
+++ Script_01.R
@@ -0,0 +1,9 @@
+###########################################################
+##########       Indicadores de mortalidad       ##########
+###########################################################
+
+library(readxl)
+library(ggplot2)
+library(ggvis)
+
+read_excel("datos_poblacion.xlsx", sheet = 1)
