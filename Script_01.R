be53b61b2bc8c44578306dad60bad3fe31f718ab
Dave9106 <david.ortiz91@hotmail.com>
2017-04-04 12:56:31 +0200

    hh

--- Script_01.R
+++ Script_01.R
@@ -1,13 +1,20 @@
-###########################################################
-##########       Indicadores de mortalidad       ##########
-###########################################################
+library(pyramid)
+
+
+datos<-data.frame(H1,M1,edad)
+pyramid(datos,Llab="Hombres",Rlab="Mujeres",Clab="Edad",main="Población Colombia
+        2005 \n (en miles)",Lcol="green", Rcol="cyan", Cgap=0.5)
+
+
 
 library(readxl)
-library(ggplot2)
-library(ggvis)
+ls('package:readxl')
+data <- read_excel("datos_poblacion.xlsx", sheet=2)[,1:3]
+pyramid(data,Llab="Hombres",Rlab="Mujeres",Clab="Edad",main="Población Alicante
+        2016",Lcol="red", Rcol="blue", Cgap=0.5,GL=F)
 
-data <- read_excel("datos_poblacion.xlsx", sheet = 1)
-colnames(data)
-
-ggplot(data, aes(Año, Alicante)) + geom_line() +
-      xlab("Año") + ylab("Personas")
+library(readxl)
+ls('package:readxl')
+data1<- read_excel("datos_poblacion.xlsx", sheet=2)[,5:7]
+pyramid(data1,Llab="Hombres",Rlab="Mujeres",Clab="Edad",main="Población Alicante
+        2001",Lcol="red", Rcol="blue", Cgap=0.5,GL=F)
