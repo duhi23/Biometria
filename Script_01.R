ba46384d24d4f9358d4ace828f9d0684e2023dc8
duhi23 <diego.21.phs@hotmail.com>
2017-04-04 23:21:40 +0200

    Changes

--- Script_01.R
+++ Script_01.R
@@ -11,3 +11,14 @@ colnames(data)
 
 ggplot(data, aes(Año, Alicante)) + geom_line() +
       xlab("Año") + ylab("Personas")
+
+
+
+load("datos.RData")
+ggplot(datos, aes(x=Año, y=Crec_absoluto, fill=Datos)) + geom_line() + geom_point(size=4, shape=21) + xlab("Año") + ylab("Crecimiento Absoluto") + scale_y_continuous(breaks=seq(0,6000000,1500000))
+
+geom_bar(stat = "identity", fill="lightblue", colour="black") 
++ geom_line() + geom_point(size=4, shape=21)
++ geom_bar(stat="identity", position="dodge")
+
+
